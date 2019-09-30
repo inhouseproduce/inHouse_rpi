5185c4c22ab16852e402911cbb2691cf0252cd4a
ocasta181 <ocasta181@gmail.com>
2019-09-30 14:07:02 -0700

    more debugging

--- set_lights.py
+++ set_lights.py
@@ -10,6 +10,11 @@ def main():
     now = datetime.datetime.now()
     today6am = now.replace(hour=6, minute=0, second=0, microsecond=0)
     today10pm = now.replace(hour=22, minute=0, second=0, microsecond=0)
+    print('now: ',now)
+    print('today6am: ',today6am)
+    print('today10pm: ',today10pm)
+    print('now >= today6am: ',now >= today6am)
+    print('now < today10pm: ',now < today10pm)
     if now >= today6am and now < today10pm:
         gp.output(pin, False)
         print(gp.input(pin))
