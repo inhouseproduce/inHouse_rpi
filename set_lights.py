c05356b82defd0a34e84d2933fa5d9939ec7ef01
ocasta181 <ocasta181@gmail.com>
2019-09-30 13:09:33 -0700

    fix organization

--- set_lights.py
+++ set_lights.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as gp
+import datetime
 
 
 def main():
@@ -6,10 +7,9 @@ def main():
     gp.setmode(gp.BOARD)
     gp.setup(pin, gp.OUT)
 
-    import datetime
-     now = datetime.datetime.now()
-     today6am = now.replace(hour=6, minute=0, second=0, microsecond=0)
-     today10pm = now.replace(hour=22, minute=0, second=0, microsecond=0)
+    now = datetime.datetime.now()
+    today6am = now.replace(hour=6, minute=0, second=0, microsecond=0)
+    today10pm = now.replace(hour=22, minute=0, second=0, microsecond=0)
     if now >= today8am and now < today10pm:
         gp.output(pin, True)
     else:
