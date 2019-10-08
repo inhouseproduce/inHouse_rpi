16cf8de13ee0c624b67ca67f8c61da788b8375e8
donnyhyun <donnyhyun288@gmail.com>
2019-10-08 16:51:57 -0700

    Changed directory name on s3

--- img_capture.py
+++ img_capture.py
@@ -14,6 +14,7 @@ import datetime
 import json
 
 import rpi_config as rpi_config
+
 #import climate as climate
 
 
--- init.py
+++ init.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 import set_cron as set_cron
+import os
 
+#install s3
+os.system("sudo apt-get -y install s3cmd")
 set_cron.main()
