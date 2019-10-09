82dd92f5dab5a8be96648b91c32cb5157422f386
ocasta181 <ocasta181@gmail.com>
2019-10-09 15:28:39 -0700

    more pythonic file open for climate.py

--- climate.py
+++ climate.py
@@ -31,33 +31,32 @@ def read_temp():
         return 'Water Temperature in Celsius is : %.1f C\nWater Temperature in Farenheit is : %.1f F\n' %(temp_c,temp_f)
 
 def write_climate(filename):
-    climate_file = open('%s' %filename, 'w+')
-    # climate_file.write(read_temp())
+    with open('%s' %filename, 'w+') as climate_file:
+        # climate_file.write(read_temp())
 
-    # Get I2C bus
-    bus = smbus.SMBus(1)
+        # Get I2C bus
+        bus = smbus.SMBus(1)
 
-    # SHT31 address, 0x44(68)
-    bus.write_i2c_block_data(0x44, 0x2C, [0x06])
+        # SHT31 address, 0x44(68)
+        bus.write_i2c_block_data(0x44, 0x2C, [0x06])
 
-    time.sleep(0.5)
+        time.sleep(0.5)
 
-    # SHT31 address, 0x44(68)
-    # Read data back from 0x00(00), 6 bytes
-    # Temp MSB, Temp LSB, Temp CRC, Humididty MSB, Humidity LSB, Humidity CRC
-    data = bus.read_i2c_block_data(0x44, 0x00, 6)
+        # SHT31 address, 0x44(68)
+        # Read data back from 0x00(00), 6 bytes
+        # Temp MSB, Temp LSB, Temp CRC, Humididty MSB, Humidity LSB, Humidity CRC
+        data = bus.read_i2c_block_data(0x44, 0x00, 6)
 
-    # Convert the data
-    temp = data[0] * 256 + data[1]
-    cTemp = -45 + (175 * temp / 65535.0)
-    fTemp = -49 + (315 * temp / 65535.0)
-    humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+        # Convert the data
+        temp = data[0] * 256 + data[1]
+        cTemp = -45 + (175 * temp / 65535.0)
+        fTemp = -49 + (315 * temp / 65535.0)
+        humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
 
-    # Output data to file
-    climate_file.write("Temperature in Celsius is : %.1f C\n" %cTemp)
-    climate_file.write("Temperature in Fahrenheit is : %.1f F\n" %fTemp)
-    climate_file.write("Relative Humidity is : %.1f %%RH\n" %humidity)
-    climate_file.close()
+        # Output data to file
+        climate_file.write("Temperature in Celsius is : %.1f C\n" %cTemp)
+        climate_file.write("Temperature in Fahrenheit is : %.1f F\n" %fTemp)
+        climate_file.write("Relative Humidity is : %.1f %%RH\n" %humidity)
 
 
 def main(pathway, date):
