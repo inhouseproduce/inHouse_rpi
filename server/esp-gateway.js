997d3ffd0a1a3bfc4c025eff781407d097bc540c
donnyhyun <donnyhyun288@gmail.com>
2019-10-10 15:59:54 -0700

    Pi updates address on config file

--- server/esp-gateway.js
+++ server/esp-gateway.js
@@ -11,10 +11,18 @@ app.post('/camera/:id', function (req, res) {
     let address = req.body['address']
     console.log('id: ',id)
     console.log('address: ',address)
-    fs.readFile('/home/pi/inHouse_rpi/config.json', 'json', function(err, data) {
+    fs.readFile('/home/pi/inHouse_rpi/config.json', 'utf8', function(err, data) {
         console.log('fs data: ',data)
+        var config = JSON.parse(data);
+        let stack_num = Math.floor((id - 1) / 6);
+        let module_num = Math.floor(((id - 1) % 6) / 2);
+        let camera_num = (id - 1) % 2;
+        config.stacks[stack_num].modules[module_num].cameras[camera_num].host = address;
+        fs.writeFile('/home/pi/inHouse_rpi/config.json', JSON.stringify(config), (err) => {
+        	console.log(err)
+        });
     })
-    res.send('new address '+ address +' recieved for camera '+id)
+    res.send('new address '+ address +' received for camera ' + id)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
