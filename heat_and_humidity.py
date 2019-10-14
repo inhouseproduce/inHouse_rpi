9548fc853962450a13f5de63fddd17a00a3db2b8
ocasta181 <ocasta181@gmail.com>
2019-10-13 17:12:08 -0700

    default temp/humid values

--- heat_and_humidity.py
+++ heat_and_humidity.py
@@ -36,7 +36,7 @@ def main():
     setPin(heatPin)
     setPin(humidPin)
 
-    cTemp, fTemp, humidity
+    cTemp, fTemp, humidity = 0
 
     try:
         cTemp, fTemp, humidity = climate.get_climate()
