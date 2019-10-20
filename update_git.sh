b1bc52ea1db28e58e9ba5be907e792eb8bdd2291
ocasta181 <ocasta181@gmail.com>
2019-10-20 16:03:33 -0700

    fixing ci/cd

--- update_git.sh
+++ update_git.sh
@@ -1,20 +1,4 @@
 #!/bin/sh
 
-# See https://stackoverflow.com/questions/3258243/check-if-pull-needed-in-git/25109122
-
-UPSTREAM=${1:-'@{u}'}
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
-
-if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
-elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
-    git pull origin raspbian-build
-    python3 init.py
-elif [ $REMOTE = $BASE ]; then
-    echo "Error codebase changed!"
-else
-    echo "Diverged"
-fi
+git pull origin raspbian-build
+python3 init.py
