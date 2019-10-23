7d2ab39881ba12183999ea261548b740e107e1de
ocasta181 <ocasta181@gmail.com>
2019-10-22 21:43:24 -0700

    top level code isn't run after an import

--- light_intensity.py
+++ light_intensity.py
@@ -1,22 +1,27 @@
 import RPi.GPIO as gp
 import time
 
-gp.setwarnings(False)
-gp.setmode(gp.BCM)
-gp.setup(21,gp.OUT)
-p = gp.PWM(21,100)
-p.start(0)
+# gp.setwarnings(False)
+# gp.setmode(gp.BCM)
+# gp.setup(21,gp.OUT)
+# p = gp.PWM(21,100)
+# p.start(0)
 
 
-def cool_down(interval):
-    for x in range (100):
-        p.ChangeDutyCycle(100-x)
-        time.sleep(interva/100)
+# def cool_down(interval):
+#     for x in range (100):
+#         p.ChangeDutyCycle(100-x)
+#         time.sleep(interva/100)
 
-def warm_up(interval):
-    for x in range (100):
-        p.ChangeDutyCycle(x)
-        time.sleep(interval/100)
+# def warm_up(interval):
+#     for x in range (100):
+#         p.ChangeDutyCycle(x)
+#         time.sleep(interval/100)
 
 def set_duty_cycle(percent):
+    gp.setwarnings(False)
+    gp.setmode(gp.BCM)
+    gp.setup(21,gp.OUT)
+    p = gp.PWM(21,100)
+    p.start(0)
     p.ChangeDutyCycle(percent)
