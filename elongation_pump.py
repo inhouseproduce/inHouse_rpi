d9f5180575ff13866840b29cdfb4236805dddebe
ocasta181 <ocasta181@gmail.com>
2019-10-23 13:14:52 -0700

    bcm mode across relays

--- elongation_pump.py
+++ elongation_pump.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
 import RPi.GPIO as gp
 import datetime
-import time
 
-#every 6 minutes for 1 minute
+# every 4 hours for 5 minutes
 def main():
-    pin = 17
+    pin = 27
     gp.setmode(gp.BCM)
     gp.setwarnings(False)
     gp.setup(pin, gp.OUT)
 
     now = datetime.datetime.now()
-    if now.minute % 6 == 0:
+    if now.hour % 4 == 0 and now.minute < 5:
         gp.output(pin, False)
     else:
         gp.output(pin, True)
-
-def trigger():
-    gp.output(pin, False)
-
-def kill():
-    gp.output(pin, True)
 
 if __name__ == "__main__":
     main()
