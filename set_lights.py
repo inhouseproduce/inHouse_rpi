d0430b480626e487f066cd2207a566c7040d90d9
Trevor Hudson <trevhud@gmail.com>
2019-10-28 14:18:27 -0700

    breaking things

--- set_lights.py
+++ set_lights.py
@@ -14,12 +14,17 @@ def main():
     if now.hour >= 6 and now.hour < 22:
         gp.output(pin, True)
         if now.hour >= 11:
-            set_brightness('25')
+            if get_brightness() != '25':
+                set_brightness('25')
         else:
             set_brightness('100')
     else:
         gp.output(pin, False)
 
+def get_brightness():
+    brightness_path = "/home/pi/inHouse_rpi/brightness.lvl"
+    with open('%s' %brightness_path, 'w') as brightness_file:
+        return brightness_file.read
 
 def set_brightness(value):
     brightness_path = "/home/pi/inHouse_rpi/brightness.lvl"
