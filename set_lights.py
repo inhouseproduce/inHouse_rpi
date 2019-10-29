afdbbd79711b3e45a7ccaa22fa9aef82349ebec8
Trevor Hudson <trevhud@gmail.com>
2019-10-28 22:34:48 -0700

    don’t set if we don’t need to

--- set_lights.py
+++ set_lights.py
@@ -5,7 +5,8 @@ import datetime
 def run(now, pin):
     # On between 10am and 2am
     if now.hour >= 10 or now.hour < 2:
-        gp.output(pin, True)
+        if gp.input(pin) == False:
+            gp.output(pin, True)
         if now.hour >= 18:
             set_brightness('30')
         else:
