c7e1c1e28e99fbd0bdaaf2e0399ed8b221c03a6d
ocasta181 <ocasta181@gmail.com>
2019-10-28 21:03:24 -0400

    Revert "breaking more stuff"
    This reverts commit 63825d8418cef1435375fd01f1f900630e67df11.

--- set_lights.py
+++ set_lights.py
@@ -11,15 +11,10 @@ def main():
     now = datetime.datetime.now()
 
     # Off between 10pm and 6am
-    do_time_check()
-
-def do_time_check():
     if now.hour >= 6 and now.hour < 22:
         gp.output(pin, True)
         if now.hour >= 11:
-            if get_brightness() == '25':
-                return
-            else:
+            if get_brightness() != '25':
                 set_brightness('25')
         else:
             set_brightness('100')
