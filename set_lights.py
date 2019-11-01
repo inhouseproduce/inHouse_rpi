5b3b6576d857e66e7339ca8108a74b91c4e132c6
Trevor Hudson <trevhud@gmail.com>
2019-10-31 17:59:57 -0700

    testing

--- set_lights.py
+++ set_lights.py
@@ -5,20 +5,24 @@ import time
 
 def run(now, pin):
     # Off between 10pm and 6am
-    if now.hour >= 6 and now.hour < 17 and now.minute < 45:
+    print('run')
+    if now.hour >= 6 and now.hour < 17 and now.minute < 57:
         gp.output(pin, True)
+        print('true')
         # if now.hour >= 11:
         #     set_brightness('25')
         # else:
         #     set_brightness('100')
     else:
-        gp.output(pin, False)   
+        gp.output(pin, False)
+        print('false')
 
 def main():
     pin = 22
     gp.setmode(gp.BCM)
     gp.setup(pin, gp.OUT)
     while(True):
+        print('while true')
         now = datetime.datetime.now()
         run(now, pin)
         time.sleep(60)
