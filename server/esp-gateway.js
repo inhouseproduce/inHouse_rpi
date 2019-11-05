b45603459c407fbd3574b7c74194b08385d7190a
donnyhyun <donnyhyun288@gmail.com>
2019-11-05 14:31:25 -0800

    create pathway to s3

--- server/esp-gateway.js
+++ server/esp-gateway.js
@@ -3,6 +3,11 @@ const bodyParser = require("body-parser")
 const fs = require('fs')
 const app = express()
 const port = 3000
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+})
 
 app.use(bodyParser.json())
 
@@ -29,15 +34,33 @@ app.post('/germination/', (req, res) => {
     console.log("New POST request detected")
     let data = req.body
     console.log(data)
-    // let body = req.body
-    // fs.writeFile('/home/pi/inHouse_rpi/germination_reading.txt', JSON.stringify(body), (err) => {
-    //     console.log(err)
-    // });
-    // let params = {
-    //     Bucket: "inhouseproduce-sites"
-    // }
+    fs.writeFile('/home/pi/inHouse_rpi/germination_reading.txt', JSON.stringify(body), (err) => {
+        console.log(err)
+    });
+    fs.readFile('/home/pi/inHouse_rpi/config.json', 'utf8', (err, data) => {
+        let config = JSON.parse(data)
+        let sitename = config.site
+        let system = config.system
 
-    res.send('New germination reading received and uploaded to server.')
+        let today = new Date();
+        let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        let datetime = date+'_'+time;
+
+        let filename = 'germination_reading_${datetime}.txt'
+        let key = '${sitename}/system${system}/${filename}'
+        // let params = {
+        //     Bucket: "inhouseproduce-sites"
+        //     Key: key
+        //     Body: JSON.stringify(body, null, 2)
+        // }
+        // s3.upload(params, (err, data) => {
+        //     console.log(err)
+        // })
+        console.log(key)
+    })
+    
+    res.send('New germination reading received and uploaded to S3.')
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
