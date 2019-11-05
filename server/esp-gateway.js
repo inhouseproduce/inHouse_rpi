99ec97410a980d14fd0bf0dfa0cf1f659bf8420a
donnyhyun <donnyhyun288@gmail.com>
2019-11-05 14:32:37 -0800

    create pathway to s3 and comment out s3 object

--- server/esp-gateway.js
+++ server/esp-gateway.js
@@ -4,10 +4,10 @@ const fs = require('fs')
 const app = express()
 const port = 3000
 const AWS = require('aws-sdk');
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-})
+// const s3 = new AWS.S3({
+//     accessKeyId: process.env.AWS_ACCESS_KEY
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+// })
 
 app.use(bodyParser.json())
 
