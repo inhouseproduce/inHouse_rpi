5f9d6f0f9a310cc09a31322f1ffdd4ea69dcd438
Trevor Hudson <trevhud@gmail.com>
2019-11-08 19:36:55 -0800

    no dim

--- set_lights.py
+++ set_lights.py
@@ -7,10 +7,10 @@ def run(now, pin):
     # Off between 10pm and 6am
     if now.hour >= 6 and now.hour < 22:
         gp.output(pin, True)
-        if now.hour >= 11:
-            set_brightness('15')
-        else:
-            set_brightness('100')
+        # if now.hour >= 11:
+        #     set_brightness('15')
+        # else:
+        #     set_brightness('100')
     else:
         gp.output(pin, False)
 
