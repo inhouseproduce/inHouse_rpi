8635b02fabdad2b6e3554c897dadc1f3e7798112
donnyhyun <donnyhyun288@gmail.com>
2019-11-12 13:38:10 -0800

    Delete local file after upload

--- server/esp-gateway.js
+++ server/esp-gateway.js
@@ -30,19 +30,19 @@ app.post('/camera/:id', (req, res) => {
 
 app.post('/germination/', (req, res) => {
     console.log("New POST request detected")
-    let data = req.body
-    console.log(data)
+    let body = req.body
+    console.log(body)
     let today = new Date();
     let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
     let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     let datetime = date+'_'+time;
-    let filename = 'germination_reading_' + datetime + '.txt'   // the filename of the germination data
+    let filename = 'germination_readings_' + datetime + '.txt'   // the filename of the germination reading
 
-    fs.writeFile('/home/pi/inHouse_rpi/' + filename, JSON.stringify(data), (err) => {
+    fs.writeFile('/home/pi/germination/' + filename, JSON.stringify(body), (err) => {
         console.log(err)
     });
     fs.readFile('/home/pi/inHouse_rpi/config.json', 'utf8', (err, data) => {
-        let config = JSON.parse(data)
+        let config = JSON.parse(body)
         let sitename = config.site
         let system = config.system
 
@@ -50,11 +50,15 @@ app.post('/germination/', (req, res) => {
         let params = {
             Bucket: "inhouseproduce-sites",
             Key: key,
-            Body: JSON.stringify(data, null, 2)
+            Body: JSON.stringify(body, null, 2)
         }
         s3.putObject(params, (err, data) => {
             console.log(err)
         })
+        console.log("Update successful.")
+    })
+    fs.unlink('/home/pi/germination/' + filename, (err) => {
+        console.log(err)
     })
     
     res.send('New germination reading received and uploaded to S3.')
