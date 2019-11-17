89fdf0be067fb299dd9c2c21d2d0fb54c2dea77b
Trevor Hudson <trevhud@gmail.com>
2019-11-16 22:33:13 -0800

    ozone logic

---
+++ ozone.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import RPi.GPIO as gp
+import datetime
+import time
+
+
+def run(now, pin):
+    if now.hour == 0 or now.hour == 3 or now.hour == 6:
+        gp.output(pin, False)
+    else:
+        gp.output(pin, True)
+
+# every 4 hours for 5 minutes
+def main():
+    pin = 22
+    gp.setmode(gp.BCM)
+    gp.setup(pin, gp.OUT)
+
+    while(True):
+        now = datetime.datetime.now()
+        run(now, pin)
+        time.sleep(60)
+
+def trigger():
+    gp.output(pin, False)
+
+def kill():
+    gp.output(pin, True)
+
+if __name__ == "__main__":
+    main()
