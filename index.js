c4ad7ca3c546d4dc2eaa4452aa8f41bcce6b62e0
Eduard Jacobs <edoter@gmail.com>
2019-11-22 17:46:38 -0800

    messy code

--- index.js
+++ index.js
@@ -1,37 +1,67 @@
 const scheduler = require('./scheduler');
-let { mainPump, secondPump, lighting } = require('./config.json');
+const config = require('./config.json');
+// const CronJob = require('cron').CronJob;
+// const GPIO = require('gpio');
 
+//let { mainPump, secondPump, lighting } = config.schedule;
 
-scheduler[mainPump.type](mainPump, { 
-    on: () => {
-        console.log('main pump is on ')
-    },
+// const dataLogger = ( name, onoff ) => {
+//     let hour = new Date().getHours();
+//     let minutes = new Date().getMinutes();
+//     let seconds = new Date().getSeconds();
+
+//     console.log('========================')
+//     console.log(`${name} is ${onoff}`,hour, minutes, seconds)
+//     console.log('========================')
+// }
+
+// scheduler[mainPump.type](mainPump, { 
+//     on: () => {
+//         dataLogger('mainpump', 'on')
+//     },
         
-    off: () => {
-        console.log('main pump is off ')
-    }
+//     off: () => {
+//         dataLogger('mainpump', 'off')
+//     }
+// });
+
+
+// scheduler[secondPump.type](secondPump, { 
+//     on: () => {
+//         console.log('secondery pump is on ')
+//     },
+        
+//     off: () => {
+//         console.log('secondery pump is off ')
+//     }
+// });
+
+
+// scheduler[lighting.type](lighting, { 
+//     on: () => {
+//         dataLogger('lighting', 'on')
+//     },
+        
+//     off: () => {
+//         dataLogger('lighting', 'off')
+//     }
+// });
+
+console.log('code is ready')
+
+const CronJob = require('cron').CronJob;
+const GPIO = require('gpio');
+
+let gpio = GPIO.export(22, {
+    direction: GPIO.DIRECTION.OUT,
 });
 
-
-scheduler[secondPump.type](secondPump, { 
-    on: () => {
-        console.log('secondery pump is on ')
-    },
-        
-    off: () => {
-        console.log('secondery pump is off ')
-    }
-});
-
-
-scheduler[lighting.type](lighting, { 
-    on: () => {
-        console.log('lighting  is on ')
-    },
-        
-    off: () => {
-        console.log('lighting  is off ')
-    }
-});
-
+new CronJob(`0 */1 * * * *`, () => {
+    console.log('off')
+    gpio.set(0);
+    setTimeout(() => { 
+        console.log('on')
+        gpio.set()
+    }, 30000);
+}).start();
 
--- scheduler/index.js
+++ scheduler/index.js
@@ -3,41 +3,62 @@ const GPIO = require('gpio');
 const CronJob = require('cron').CronJob;
 
 const timer = require('./timer');
-const checker = require('../errorlog');
+const checker = require('./errorLog');
 
 class Scheduler {
     constructor(){
+        // Interval based scheduling
         this.interval = (config, action) => {
             // Initialize pin config
             let gpio = GPIO.export(config.pin, {
                 direction: GPIO.DIRECTION[config.direction],
             });
-
-            // Turn on on the start
-            if( config.int ) 
-                this.action(config, action, gpio);
-
-            // If config requires error checker
-            if( config.errorCheck ) 
-                checker.error(config, gpio);
-            
+            gpio.reset()
             // Run interval specified in config
-            new CronJob( timer(config), () => { 
-                this.action(config, action, gpio);
+            new CronJob(`0 */${config.time_interval+1} * * * *`, () => {
+                action.on();
+                gpio.set();
+                setTimeout(() => { 
+                    action.off();
+                    gpio.set(0)
+                }, config.run_period * 60000);
 			}).start();
+        }
+
+        this.clock = (config, action) => {
+            let gpio = this.initializeGpio(config);
+
+            if( config.init )
+                this.actionSwitch(config, action, gpio );
+
+            setInterval(() => {
+                action.on
+            }, config.time_interval+1 * 60000);
         }
     }
 
-    action(config, action, gpio){
-        // Tun on int cron timer
-        action.on();
-        gpio.set(1);
+    actionSwitch( config, action, gpio ){
+        // Turn on the start
+        onoff(true);
 
-        // Turn off in run_period * miliseconds
-        setTimeout(() => { 
-            action.off();
-            gpio.set(0)
-         }, config.run_period * 60000);
+        // Turn off based on run period
+        setTimeout(() => { onoff(false) 
+        }, config.run_period * 60000);
+        
+        function onoff(onoff){
+            if( config.direction && config.pin ) 
+                gpio.set(onoff ? 1 : 0);
+
+            if( action && action.on ) 
+                action[onoff ? 'on' : 'off']();
+        }
+    }
+
+    initializeGpio( config ){
+        let gpio = GPIO.export(config.pin, {
+            direction: GPIO.DIRECTION[config.direction],
+        });
+        return gpio;
     }
 };
 
