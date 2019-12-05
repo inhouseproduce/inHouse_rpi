161342fb2b20e8f577928f726a2e4cd6f939e938
donnyhyun <donnyhyun288@gmail.com>
2019-12-05 15:06:25 -0800

    Draft

--- esp-geteaway/index.js
+++ esp-geteaway/index.js
@@ -14,28 +14,22 @@ const AWS = require('aws-sdk');
 module.exports = (app) => {
     app.post('/camera/:id', (req, res) => {
         console.log("New POST request detected")
-        let mac = req.body['id']
+        let mac = req.params.id
         console.log('MAC: ',mac)
         fs.readFile('/app/inHouse_rpi/config.json', 'utf8', (err, data) => {
             let config = JSON.parse(data)
             let id = config.esp32[mac]
             console.log('Camera ID: ',id)
-            let stack_num = Math.floor((id - 1) / 6)
-            let module_num = Math.floor(((id - 1) % 6) / 2)
-            let camera_num = (id - 1) % 2
-            // config.stacks[stack_num].modules[module_num].cameras[camera_num].host = address;
-            // fs.writeFile('/app/inHouse_rpi/config.json', JSON.stringify(config, null, 5), (err) => {
-            //     if (err) {
-            //         console.log(err)
-            //     }
-            // })
+            let stack_num = Math.floor((id - 1) / 6);
+            let module_num = Math.floor(((id - 1) % 6) / 2);
+            let camera_num = (id - 1) % 2;
             let obj = {
                 stack: stack_num,
                 module: module_num,
                 camera: camera_num
-            }
-
-            res.send("Image received and uploaded to S3.")
+            };
+            esp32(obj);
+            res.send("New image received for camera " + id + " and uploaded to S3.")
         })
     })
 
@@ -59,31 +53,31 @@ module.exports = (app) => {
             let config = JSON.parse(data)
             let sitename = config.site
             let system = config.system
-            let pathway = "s3://inhouseproduce-sites/" + sitename + "/system" + system + "/" + "germination/" + filename
-            // let pathway = sitename + "/system" + system + "/" + "germination/" + filename
-            // const params = {
-            //     Bucket: 'inhouseproduce-sites',
-            //     Key: pathway,
-            //     Body: JSON.stringify(body, null, 2)
-            // }
-            // s3.upload(params, (err, data) => {
-            //     if (err) {
-            //         console.log(err)
-            //     }
-            //     console.log(`File uploaded successfully. ${data.Location}`)
-            // })
-            cmd.get('s3cmd put ' +  filename + ' ' + pathway, (err, data) => {
+            // let pathway = "s3://inhouseproduce-sites/" + sitename + "/system" + system + "/" + "germination/" + filename
+            let pathway = sitename + "/system" + system + "/" + "germination/" + filename
+            const params = {
+                Bucket: 'inhouseproduce-sites',
+                Key: pathway,
+                Body: JSON.stringify(body, null, 2)
+            }
+            s3.upload(params, (err, data) => {
                 if (err) {
                     console.log(err)
-                } else {
-                    console.log("Update successful.")
-                    fs.unlink(filename, (err, data) => {
-                        if (err) {
-                            console.log(err)
-                        }
-                    })
                 }
+                console.log(`File uploaded successfully. ${data.Location}`)
             })
+            // cmd.get('s3cmd put ' +  filename + ' ' + pathway, (err, data) => {
+            //     if (err) {
+            //         console.log(err)
+            //     } else {
+            //         console.log("Update successful.")
+            //         fs.unlink(filename, (err, data) => {
+            //             if (err) {
+            //                 console.log(err)
+            //             }
+            //         })
+            //     }
+            // })
             res.send('New germination reading received and uploaded to S3.')
         })
     });
