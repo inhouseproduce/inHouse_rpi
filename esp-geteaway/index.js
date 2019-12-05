88ad0c905201453695267f2d7cc6bab4405aa8b3
donnyhyun <donnyhyun288@gmail.com>
2019-12-05 15:12:26 -0800

    Deleted some old lines

--- esp-geteaway/index.js
+++ esp-geteaway/index.js
@@ -53,11 +53,11 @@ module.exports = (app) => {
             let config = JSON.parse(data)
             let sitename = config.site
             let system = config.system
-            // let pathway = "s3://inhouseproduce-sites/" + sitename + "/system" + system + "/" + "germination/" + filename
-            let pathway = sitename + "/system" + system + "/" + "germination/" + filename
+            let pathway = "inhouseproduce-sites/" + sitename + "/system" + system + "/" + "germination/"
+            // let pathway = sitename + "/system" + system + "/" + "germination/" + filename
             const params = {
-                Bucket: 'inhouseproduce-sites',
-                Key: pathway,
+                Bucket: pathway,
+                Key: filename,
                 Body: JSON.stringify(body, null, 2)
             }
             s3.upload(params, (err, data) => {
@@ -66,18 +66,6 @@ module.exports = (app) => {
                 }
                 console.log(`File uploaded successfully. ${data.Location}`)
             })
-            // cmd.get('s3cmd put ' +  filename + ' ' + pathway, (err, data) => {
-            //     if (err) {
-            //         console.log(err)
-            //     } else {
-            //         console.log("Update successful.")
-            //         fs.unlink(filename, (err, data) => {
-            //             if (err) {
-            //                 console.log(err)
-            //             }
-            //         })
-            //     }
-            // })
             res.send('New germination reading received and uploaded to S3.')
         })
     });
