5d99d63282644132a92753e0e1bd06dccf02ae36
donnyhyun <donnyhyun288@gmail.com>
2019-12-10 13:34:39 -0800

    Working on buffer

--- esp-geteaway/index.js
+++ esp-geteaway/index.js
@@ -1,28 +1,46 @@
 const fs = require('fs');
 const cmd = require('node-cmd')
+const esp32 = require('../server/')
+const AWS = require('aws-sdk');
+// const s3 = new AWS.S3({
+//   accessKeyId: balena.models.application.envVar.get('inhouse-produce', 'AccessKey', function(error, value) {
+//         if (error) throw error;
+//     }),
+//   secretAccessKey: balena.models.application.envVar.get('inhouse-produce', 'SecretKey', function(error, value) {
+//         if (error) throw error;
+//     })
+// })
 
 module.exports = (app) => {
-    app.post('/camera/:id', (req, res) => {
-        let id = req.params.id;
-        let address = req.body['address'];
-        fs.readFile('/home/pi/inHouse_rpi/config.json', 'utf8', (err, data) => {
+    app.post('/camera/', (req, res) => {
+        console.log("New POST request detected")
+        // let mac = req.params.id
+        let buf = req.body
+        // console.log('MAC: ',mac)
+        console.log(buf)
+        fs.readFile('/app/inHouse_rpi/config.json', 'utf8', (err, data) => {
             let config = JSON.parse(data)
-            let stack_num = Math.floor((id - 1) / 6)
-            let module_num = Math.floor(((id - 1) % 6) / 2)
-            let camera_num = (id - 1) % 2;
-
-            config.stacks[stack_num].modules[module_num].cameras[camera_num].host = address
-            fs.writeFile('/home/pi/inHouse_rpi/config.json', 
-                JSON.stringify(config, null, 5), (err) => {
-            })
+            // let id = config.esp32[mac]
+            // console.log('Camera ID: ',id)
+            // let stack_num = Math.floor((id - 1) / 6);
+            // let module_num = Math.floor(((id - 1) % 6) / 2);
+            // let camera_num = (id - 1) % 2;
+            // let obj = {
+            //     stack: stack_num,
+            //     module: module_num,
+            //     camera: camera_num
+            // };
+            // esp32(obj);
+            // res.send("New image received for camera " + id + " and uploaded to S3.")
         })
-        res.send('new address ' + address + ' received for camera ' + id)
     })
 
     app.post('/germination/', (req, res) => {
+        console.log("New POST request detected")
         let body = req.body
+        console.log(body)
         let today = new Date()
-        let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
+        let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
         let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
         let datetime = date + '_' + time
         let filename = 'germination_readings_' + datetime + '.txt'
@@ -32,25 +50,24 @@ module.exports = (app) => {
                 console.log(err)
             }
         })
-        fs.readFile('/home/pi/inHouse_rpi/config.json', 'utf8', (err, data) => {
+
+        fs.readFile('/app/inHouse_rpi/config.json', 'utf8', (err, data) => {
             let config = JSON.parse(data)
             let sitename = config.site
             let system = config.system
-            let pathway = "s3://inhouseproduce-sites/" + sitename + "/system" + system + "/" + "germination/" + filename
-
-            cmd.get('s3cmd put ' + filename + ' ' + pathway, (err, data) => {
+            let pathway = "inhouseproduce-sites/" + sitename + "/system" + system + "/" + "germination/"
+            const params = {
+                Bucket: pathway,
+                Key: filename,
+                Body: JSON.stringify(body, null, 2)
+            }
+            s3.upload(params, (err, data) => {
                 if (err) {
                     console.log(err)
-                } else {
-                    console.log("Update successful.")
-                    fs.unlink(filename, (err, data) => {
-                        if (err) {
-                            console.log(err)
-                        }
-                    })
                 }
+                console.log(`File uploaded successfully. ${data.Location}`)
             })
+            res.send('New germination reading received and uploaded to S3.')
         })
-        res.send('New germination reading received and uploaded to S3.')
     });
 }
