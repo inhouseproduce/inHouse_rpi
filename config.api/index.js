1a7eb21597cec06c55b5557ab0a817526bf54cc2
Eduard Jacobs <edoter@gmail.com>
2019-12-11 03:34:31 -0800

    saves latest config file

--- config.api/index.js
+++ config.api/index.js
@@ -1,16 +1,70 @@
-const axios = require('axios');
-const defaultConfig = require('../device/config.json');
+const request = require('axios');
+const fs = require('fs');
 
 module.exports = async () => {
-    const url = 'https://webapp-inhouse.herokuapp.com/get/json';
+    const url = 'https://webapp-inhouse.herokuapp.com/get/son';
 
-    return await axios.get(url).then(res => {
-        return res.data;
+    return await request.get(url).then(res => {
+        // Save file than get the file and return
+        saveJsonFile(res.data);
+        return getDefaultConfig();
     })
-    .catch(error => {
-        if (error) {
-            console.log('connection error, using default config');
-            return defaultConfig;
-        }
-    });
+        .catch(error => {
+            if (error) {
+                return getDefaultConfig();
+            }
+        });
+
+    // Save json file to current direcotry
+    function saveJsonFile(data) {
+        // config file validation
+        let isValid = validateConfig(data);
+
+        if (isValid) {
+            // Convert to json and save
+            let config = JSON.stringify(data);
+            fs.writeFileSync('./config.api/saved.json', config);
+        } else {
+            console.log('Config file is not valid');
+        };
+    };
+
+    // Return valid config.json/ lastest or default
+    function getDefaultConfig() {
+        // Get saved.json config file and validate
+        let savedConfig = require('./saved.json');
+        let isValid = validateConfig(savedConfig);
+
+        if (isValid) {
+            return savedConfig;
+        };
+        // Return Default.json file
+        return require('./default.json');
+    };
+
+    // Validates config type
+    function validateConfig(config) {
+        // Validate if type is object
+        if (config && typeof config === 'object') {
+            return true;
+        };
+        return false
+    };
 };
+
+
+
+
+
+
+//**** Descritpion
+// 1. Makes a requrest to url to get json file
+// 2. If json file is valid save in saved.json file
+// 3. Then read from saved.json file and return
+// 4. If error occured get saved.json validate and return 
+// 5. If saved.json file is not valid than get default default.json
+
+//* 3 functions */
+// 1. saveJsonFile --- validates and saves file to saved.json
+// 2. getDefaultConfig --- first read saved.json file if valid return else return           default.json file
+// 3. validateConfig --- validates if type is object
--- start 2.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-# Default to UTC if no TIMEZONE env variable is set
-echo "Setting time zone to Europe/London"
-# This only works on Debian-based images
-echo Europe/London > /etc/timezone dpkg-reconfigure tzdata
