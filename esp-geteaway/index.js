d89cb4faf2558a9d939d229caadd3497ede87950
donnyhyun <donnyhyun288@gmail.com>
2019-12-10 16:05:23 -0800

    Best version currently

--- esp-geteaway/index.js
+++ esp-geteaway/index.js
@@ -13,31 +13,36 @@ const AWS = require('aws-sdk');
 module.exports = (app) => {
     app.post('/camera/', (req, res) => {
         console.log("New POST request detected")
-        // let mac = req.body.id
+        // console.log(req.body)
+        let mac = req.body.id
         let buf = req.body.image
-        // console.log('MAC: ',mac)
-        console.log(buf)
-        var b64encoded = btoa(String.fromCharCode.apply(null, new Uint8Array(buf)));
-        var datajpg = "data:image/jpg;base64," + b64encoded;
+        console.log('MAC: ',mac)
+        console.log('Buf: ',buf)
+        // converts buffer to base64 and then convert that into an image file
+        let b64encoded = btoa(String.fromCharCode.apply(null, new Uint8Array(buf)));
+        let datajpg = "data:image/jpg;base64," + b64encoded;
         let base64Image = datajpg.split(';base64,').pop();
         fs.writeFile('image.png', base64Image, {encoding: 'base64'}, function(err) {
             console.log('File created');
         });
-        // fs.readFile('/app/config.json', 'utf8', (err, data) => {
-        //     let config = JSON.parse(data)
-            // let id = config.camera[mac]
-            // console.log('Camera ID: ',id)
-            // let stack_num = Math.floor((id - 1) / 6);
-            // let module_num = Math.floor(((id - 1) % 6) / 2);
-            // let camera_num = (id - 1) % 2;
-            // let obj = {
-            //     stack: stack_num,
-            //     module: module_num,
-            //     camera: camera_num
-            // };
-            // esp32(obj);
-            // res.send("New image received for camera " + id + " and uploaded to S3.")
-        // })
+        fs.readFile('/app/device/config.json', 'utf8', (err, data) => {
+            let config = JSON.parse(data)
+            let id = config.camera[mac]
+            console.log('Camera ID: ',id)
+            let stack_num = Math.floor((id - 1) / 6);
+            let module_num = Math.floor(((id - 1) % 6) / 2);
+            let camera_num = (id - 1) % 2;
+            let content = fs.readFileSync('image.png')
+            let obj = {
+                stack: stack_num,
+                module: module_num,
+                camera: camera_num,
+                name: 'image.jpg',
+                data: content
+            };
+            esp32(obj);
+            res.send("New image received for camera " + id + " and uploaded to S3.")
+        })
     })
 
     app.post('/germination/', (req, res) => {
@@ -56,7 +61,7 @@ module.exports = (app) => {
             }
         })
 
-        fs.readFile('/app/inHouse_rpi/config.json', 'utf8', (err, data) => {
+        fs.readFile('/app/device/config.json', 'utf8', (err, data) => {
             let config = JSON.parse(data)
             let sitename = config.site
             let system = config.system
@@ -70,6 +75,11 @@ module.exports = (app) => {
                 if (err) {
                     console.log(err)
                 }
+                fs.unlink(filename, (err) => {
+                    if (err) {
+                        console.log(err)
+                    }
+                })
                 console.log(`File uploaded successfully. ${data.Location}`)
             })
             res.send('New germination reading received and uploaded to S3.')
